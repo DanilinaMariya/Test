93410d9edf6729397d51798b79f51092769df368
Danilina Mariya <mariya199828@gmail.com>
2024-06-30 23:43:41 +0700

    Рефакторинг скрипта карт

--- main.js
+++ main.js
@@ -1,48 +1,52 @@
-let deckId = '4yu6wyyvb2y2';
+let deckId;
 const cards = document.querySelectorAll('.card');
 const buttonReset = document.querySelector('.button-reset')
 
-// async function getData() {
-//     const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,AC,AH,AD";
-//     try {
-//       const response = await fetch(url);
-//       if (!response.ok) {
-//         throw new Error(`Response status: ${response.status}`);
-//       }  
-//       const json = await response.json();
-//       deckId = json.deck_id;
-//       console.log(json, deckId);
-//     } catch (error) {
-//       console.error(error.message);
-//     }
-//   };
+let URL = {
+    deck: "https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,AC,AH,AD",
+}
 
-//   getData();
+const api = async (url) => {
+    try {
+            const response = await fetch(url);
+            if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+            }  
+            const data = await response.json();
+            return data;
+
+        } catch (error) {
+            console.error(error.message);
+        }
+}
+
+async function getData() {
+    const data = await api(URL.deck);
+    deckId = data.deck_id;
+    URL = {
+        card: `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`,
+        shuffleDesk: `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`,
+        returnCard:`https://deckofcardsapi.com/api/deck/${deckId}/return/`,
+    }
+};
+
+getData();
 
 async function onCardClick(el) {
     if(el.classList.contains('card--pickUp')) {
         return;
     }
-    const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
     const img = el.querySelector('.card__front').querySelector('img')
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-        }  
-        const json = await response.json();
-        const imageSrc = json.cards[0].image;
-        img.setAttribute('src', `${imageSrc}`);
-        if (el.classList.contains('card--pickDown')) {
-                el.classList.remove('card--pickDown');
-                el.classList.add('card--pickUp');
-            }
-        if (json.remaining === 0) {
-        buttonReset.style.opacity = '1';
+    const data = await api(URL.card);
+    const imageSrc = data.cards[0].image;
+    img.setAttribute('src', `${imageSrc}`);
+    if (el.classList.contains('card--pickDown')) {
+            el.classList.remove('card--pickDown');
+            el.classList.add('card--pickUp');
         }
-    } catch (error) {
-        console.error(error.message);
-}
+    if (data.remaining === 0) {
+    buttonReset.style.opacity = '1';
+    }
 };
 
 function onCardClose(i) {
@@ -51,34 +55,22 @@ function onCardClose(i) {
 }
 
 async function shuffleDeck() {
-    const url = `https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`;
-    try {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-    }  
-    } catch (error) {
-    console.error(error.message);
-    }
+    api(URL.shuffleDesk)
 }
 
 
 async function onButtonResetClick() {
-    buttonReset.style.opacity = '0';
-    setTimeout(onCardClose, 0, 0);
-    setTimeout(onCardClose, 200, 1);
-    setTimeout(onCardClose, 400, 2);
-    setTimeout(onCardClose, 600, 3);
-    const url = `https://deckofcardsapi.com/api/deck/${deckId}/return/`;
-    shuffleDeck();
-    try {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-    }  
-    } catch (error) {
-    console.error(error.message);
+    const data = await api(URL.returnCard);
+    if (data.success) {
+        buttonReset.style.opacity = '0';
+        setTimeout(onCardClose, 0, 0);
+        setTimeout(onCardClose, 200, 1);
+        setTimeout(onCardClose, 400, 2);
+        setTimeout(onCardClose, 600, 3);
+    
+        shuffleDeck();
     }
+
 }
 
 cards.forEach((el) => {
